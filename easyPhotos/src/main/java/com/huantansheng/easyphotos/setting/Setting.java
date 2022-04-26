2c6c91e71df8e7a65f90f95fc511c7e531541e66
边胤铮 <81094852@qq.com>
2022-04-26 10:28:50 +0800

    边-新增滑动多选V1.3.8

--- easyPhotos/src/main/java/com/huantansheng/easyphotos/setting/Setting.java
+++ easyPhotos/src/main/java/com/huantansheng/easyphotos/setting/Setting.java
@@ -95,6 +95,7 @@ public class Setting {
         complexVideoCount = 0;
         complexPictureCount = 0;
         isSequentialSelectedPhotos = true;
+        slide = false
     }
 
     public static boolean isFilter(String type) {
